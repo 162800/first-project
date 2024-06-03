b545c2bde4ec6513f0d7291ee1a42ac2c56abbeb
Andiego <dandyandiego002@gmail.com>
2024-06-03 17:43:42 +0300

    Update and rename README.md to value of students in class.c
    basic c program to show the accummilative value of students in class

---
+++ value of students in class.c
@@ -0,0 +1,17 @@
+# value-of-students-in-clas//value of students in class
+#include<stdio.h>
+int main()
+{
+    int boys = 15;
+    float girls = 5;
+    double absent = 7;
+    char s = 'u';
+    char v[] = "online";
+    
+    printf("the number of character %c\n", s);
+    printf("the number of string %c\n", v);
+    printf("the number of boys %d \n", boys);
+    printf("the number of girls %.0f\n", girls);
+    printf("the number of absent %.0lf", absent);
+    
+    return 0;
